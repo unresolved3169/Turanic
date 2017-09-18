3d76fa1fbd80edd592bc42670ee1b10cce0fe967
Ip <jun040329@gmail.com>
2017-09-18 17:05:36 +0900

    Update Enderman.php

--- src/pocketmine/entity/Enderman.php
+++ src/pocketmine/entity/Enderman.php
@@ -21,8 +21,12 @@
 
 namespace pocketmine\entity;
 
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\Item as ItemItem;
 use pocketmine\network\mcpe\protocol\AddEntityPacket;
 use pocketmine\Player;
+use pocketmine\entity\behavior\{StrollBehavior, RandomLookaroundBehavior, LookAtPlayerBehavior, PanicBehavior};
 
 class Enderman extends Monster {
 	const NETWORK_ID = 38;
