a3d7314da5f2d070608a565add36a961f5fd8696
Emre YavuzYiğit <emre.yavuz.842@gmail.com>
2017-10-09 20:45:45 +0300

    Upload Stained Glass files

---
+++ src/pocketmine/block/StainedGlass.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ *
+ *    _______                    _
+ *   |__   __|                  (_)
+ *      | |_   _ _ __ __ _ _ __  _  ___
+ *      | | | | | '__/ _` | '_ \| |/ __|
+ *      | | |_| | | | (_| | | | | | (__
+ *      |_|\__,_|_|  \__,_|_| |_|_|\___|
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author TuranicTeam
+ * @link https://github.com/TuranicTeam/Turanic
+ *
+*/
+
+namespace pocketmine\block;
+
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\Item;
+use pocketmine\block\utils\ColorBlockMetaHelper;
+
+class StainedGlass extends Transparent {
+
+	protected $id = self::STAINED_GLASS;
+
+	/**
+	 * StainedGlass constructor.
+	 */
+	public function __construct($meta = 0){
+		$this->meta = $meta;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName() : string{
+		return ColorBlockMetaHelper::getColorFromMeta($this->meta) . " Stained Glass";
+	}
+
+	/**
+	 * @return float
+	 */
+	public function getHardness(){
+		return 0.3;
+	}
+
+	/**
+	 * @param Item $item
+	 *
+	 * @return array
+	 */
+	public function getDrops(Item $item) : array{
+		if($item->getEnchantmentLevel(Enchantment::TYPE_MINING_SILK_TOUCH) > 0){
+			return [
+				[Item::STAINED_GLASS, $this->meta, 1],
+			];
+		}else{
+			return [];
+		}
+	}
+}
