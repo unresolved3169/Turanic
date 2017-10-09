175031ec2a2968e884a2a3b09e45b52a09a908d7
SuperKali <danychannel18@gmail.com>
2017-10-09 19:51:23 +0200

    ops, try to fix 1#

--- src/pocketmine/command/defaults/StatusCommand.php
+++ src/pocketmine/command/defaults/StatusCommand.php
@@ -41,7 +41,7 @@ class StatusCommand extends VanillaCommand{
 		);
 		$this->setPermission("pocketmine.command.status");
 	}
-	public function execute(CommandSender $sender, string $commandLabel, array $args){
+	public function execute(CommandSender $sender, $commandLabel, array $args){
 		if(!$this->testPermission($sender)){
 			return true;
 		}
