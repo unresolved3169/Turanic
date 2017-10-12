793357eb0f41359adc0609c049b6d5002e5f1e3b
Emre YavuzYiğit <emre.yavuz.842@gmail.com>
2017-10-12 16:47:20 +0300

    Fixed Transfer command parameters

--- src/pocketmine/command/defaults/TransferServerCommand.php
+++ src/pocketmine/command/defaults/TransferServerCommand.php
@@ -2,22 +2,22 @@
 
 /*
  *
- *
- *    _______                    _
- *   |__   __|                  (_)
- *      | |_   _ _ __ __ _ _ __  _  ___
- *      | | | | | '__/ _` | '_ \| |/ __|
- *      | | |_| | | | (_| | | | | | (__
- *      |_|\__,_|_|  \__,_|_| |_|_|\___|
- *
+ *  _____            _               _____           
+ * / ____|          (_)             |  __ \          
+ *| |  __  ___ _ __  _ ___ _   _ ___| |__) | __ ___  
+ *| | |_ |/ _ \ '_ \| / __| | | / __|  ___/ '__/ _ \ 
+ *| |__| |  __/ | | | \__ \ |_| \__ \ |   | | | (_) |
+ * \_____|\___|_| |_|_|___/\__, |___/_|   |_|  \___/ 
+ *                         __/ |                    
+ *                        |___/                     
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author TuranicTeam
- * @link https://github.com/TuranicTeam/Turanic
+ * @author Turanic
+ * @link https://github.com/Turanic/Turanic
  *
  *
 */
@@ -26,8 +26,6 @@ namespace pocketmine\command\defaults;
 
 use pocketmine\network\mcpe\protocol\TransferPacket;
 use pocketmine\command\CommandSender;
-use pocketmine\command\ConsoleCommandSender;
-use pocketmine\utils\TextFormat;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\command\overload\CommandParameter;
@@ -42,15 +40,14 @@ class TransferServerCommand extends VanillaCommand {
 	public function __construct($name){
 		parent::__construct(
 			$name,
-			"Send the player to another server",
-			"/transferserver <player> <address> [port]",
-			["transferserver", "transfer"]
+			"将玩家传送至另一个服务器",
+			"/transferserver <player玩家> <address地址> [port端口]",
+			["transferserver"]
 		);
 		$this->setPermission("pocketmine.command.transfer");
 		
-		$this->getOverload("default")->setParameter(0, new CommandParameter("player", CommandParameter::TYPE_TARGET, false));
-		$this->getOverload("default")->setParameter(1, new CommandParameter("address", CommandParameter::TYPE_STRING, false));
-		$this->getOverload("default")->setParameter(2, new CommandParameter("port", CommandParameter::TYPE_INT, true));
+		$this->getOverload("default")->setParameter(0, new CommandParameter("address", CommandParameter::TYPE_STRING, false));
+		$this->getOverload("default")->setParameter(1, new CommandParameter("port", CommandParameter::TYPE_INT, true));
 	}
 
 	/**
@@ -63,23 +60,47 @@ class TransferServerCommand extends VanillaCommand {
 	public function execute(CommandSender $sender, $currentAlias, array $args){
 		$address = null;
 		$port = null;
+		$player = null;
 		if($sender instanceof Player){
 			if(!$this->testPermission($sender)){
 				return true;
 			}
-			if($sender instanceof ConsoleCommandSender){
-				$sender->sendMessage(TextFormat::RED . 'A console can not be transferred!');
-				return true;
-			}
-			if(count($args) < 2 || !is_string(($address = $args[0])) || !is_numeric(($port = $args[1]))){
-				$sender->sendMessage("Usage: /transferserver <address> <port>");
+
+			if(count($args) <= 0){
+				$sender->sendMessage("Usage: /transferserver <address> [port]");
 				return false;
 			}
+
+			$address = strtolower($args[0]);
+			$port = (isset($args[1]) && is_numeric($args[1]) ? $args[1] : 19132);
+
 			$pk = new TransferPacket();
 			$pk->address = $address;
 			$pk->port = $port;
 			$sender->dataPacket($pk);
+
 			return false;
 		}
+
+		if(count($args) <= 1){
+			$sender->sendMessage("Usage: /transferserver <player> <address> [port]");
+			return false;
+		}
+
+		if(!($player = Server::getInstance()->getPlayer($args[0])) instanceof Player){
+			$sender->sendMessage("Player specified not found!");
+			return false;
+		}
+
+		$address = strtolower($args[1]);
+		$port = (isset($args[2]) && is_numeric($args[2]) ? $args[2] : 19132);
+
+		$sender->sendMessage("Sending " . $player->getName() . " to " . $address . ":" . $port);
+
+		$pk = new TransferPacket();
+		$pk->address = $address;
+		$pk->port = $port;
+		$player->dataPacket($pk);
 	}
+
 }
