63d08cdd5f390ff03a17bc68b22527cd01a53a7b
SuperKali <danychannel18@gmail.com>
2017-11-14 21:31:19 +0100

    Revert "remake code"
    This reverts commit e7020cf

--- src/pocketmine/network/mcpe/protocol/types/WindowTypes.php
+++ src/pocketmine/network/mcpe/protocol/types/WindowTypes.php
@@ -25,26 +25,26 @@ declare(strict_types=1);
 namespace pocketmine\network\mcpe\protocol\types;
 
 
-interface WindowTypes
-{
+interface WindowTypes{
 
-    const INVENTORY = -1;
-    const CONTAINER = 0;
-    const WORKBENCH = 1;
-    const FURNACE = 2;
-    const ENCHANTMENT = 3;
-    const BREWING_STAND = 4;
-    const ANVIL = 5;
-    const DISPENSER = 6;
-    const DROPPER = 7;
-    const HOPPER = 8;
-    const CAULDRON = 9;
-    const MINECART_CHEST = 10;
-    const MINECART_HOPPER = 11;
-    const HORSE = 12;
-    const BEACON = 13;
-    const STRUCTURE_EDITOR = 14;
-    const TRADING = 15;
-    const COMMAND_BLOCK = 16;
+	const INVENTORY = -1;
+	const CONTAINER = 0;
+	const WORKBENCH = 1;
+	const FURNACE = 2;
+	const ENCHANTMENT = 3;
+	const BREWING_STAND = 4;
+	const ANVIL = 5;
+	const DISPENSER = 6;
+	const DROPPER = 7;
+	const HOPPER = 8;
+	const CAULDRON = 9;
+	const MINECART_CHEST = 10;
+	const MINECART_HOPPER = 11;
+	const HORSE = 12;
+	const BEACON = 13;
+	const STRUCTURE_EDITOR = 14;
+	const TRADING = 15;
+	const COMMAND_BLOCK = 16;
     const JUKEBOX = 17;
+
 }
