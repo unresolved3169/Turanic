5fee8ed282072cf07d2c6f2a70e6a7198afb56fb
Enes Yıldırım <enes5519@gmail.com>
2017-11-23 23:53:02 +0300

    I deleted my test code

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -3448,7 +3448,6 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 	
 	public function handlePing(PingPacket $packet) : bool{
 		// TODO: Add event
-        $this->server->getLogger()->info("player handlePing: §a".$packet->ping);
 		$this->setPing($packet->ping);
 		return true;
 	}
