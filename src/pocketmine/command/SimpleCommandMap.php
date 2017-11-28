269b62fd529135091cd9cbcef593a0a1db7f8fb1
ENES <enes5519@gmail.com>
2017-11-28 19:57:47 +0300

    Clean up and add unregister function (based from pmmp) SimpleCommandMap

--- src/pocketmine/command/SimpleCommandMap.php
+++ src/pocketmine/command/SimpleCommandMap.php
@@ -120,72 +120,70 @@ class SimpleCommandMap implements CommandMap {
 	}
 
 	private function setDefaultCommands(){
-		$this->register("pocketmine", new WeatherCommand("weather"));
-
-		$this->register("pocketmine", new BanCidCommand("bancid"));
-		$this->register("pocketmine", new PardonCidCommand("pardoncid"));
-		$this->register("pocketmine", new BanCidByNameCommand("bancidbyname"));
-		$this->register("pocketmine", new BanIpByNameCommand("banipbyname"));
-
-		$this->register("pocketmine", new ExtractPharCommand("extractphar"));
-		$this->register("pocketmine", new ExtractPluginCommand("ep"));
-		$this->register("pocketmine", new MakePluginCommand("mp"));
-		$this->register("pocketmine", new MakeServerCommand("ms"));
-		$this->register("pocketmine", new LoadPluginCommand("loadplugin"));
-
-		$this->register("pocketmine", new LvdatCommand("lvdat"));
-		$this->register("pocketmine", new BiomeCommand("biome"));
-		$this->register("pocketmine", new CaveCommand("cave"));
-		$this->register("pocketmine", new ChunkInfoCommand("chunkinfo"));
-
-		$this->register("pocketmine", new VersionCommand("version"));
-		$this->register("pocketmine", new FillCommand("fill"));
-		$this->register("pocketmine", new PluginsCommand("plugins"));
-		$this->register("pocketmine", new SeedCommand("seed"));
-		$this->register("pocketmine", new HelpCommand("help"), null, true);
-		$this->register("pocketmine", new StopCommand("stop"), null, true);
-		$this->register("pocketmine", new TellCommand("tell"));
-		$this->register("pocketmine", new DefaultGamemodeCommand("defaultgamemode"));
-		$this->register("pocketmine", new BanCommand("ban"));
-		$this->register("pocketmine", new BanIpCommand("ban-ip"));
-		$this->register("pocketmine", new BanListCommand("banlist"));
-		$this->register("pocketmine", new PardonCommand("pardon"));
-		$this->register("pocketmine", new PardonIpCommand("pardon-ip"));
-		$this->register("pocketmine", new SayCommand("say"));
-		$this->register("pocketmine", new MeCommand("me"));
-		$this->register("pocketmine", new ListCommand("list"));
-		$this->register("pocketmine", new DifficultyCommand("difficulty"));
-		$this->register("pocketmine", new KickCommand("kick"));
-		$this->register("pocketmine", new OpCommand("op"));
-		$this->register("pocketmine", new DeopCommand("deop"));
-		$this->register("pocketmine", new WhitelistCommand("whitelist"));
-		$this->register("pocketmine", new SaveOnCommand("save-on"));
-		$this->register("pocketmine", new SaveOffCommand("save-off"));
-		$this->register("pocketmine", new SaveCommand("save-all"), null, true);
-		$this->register("pocketmine", new GiveCommand("give"));
-		$this->register("pocketmine", new EffectCommand("effect"));
-		$this->register("pocketmine", new EnchantCommand("enchant"));
-		$this->register("pocketmine", new ParticleCommand("particle"));
-		$this->register("pocketmine", new PingCommand("ping"));
-		$this->register("pocketmine", new GamemodeCommand("gamemode"));
-		$this->register("pocketmine", new KillCommand("kill"));
-		$this->register("pocketmine", new SpawnpointCommand("spawnpoint"));
-		$this->register("pocketmine", new SetWorldSpawnCommand("setworldspawn"));
-		$this->register("pocketmine", new SummonCommand("summon"));
-		$this->register("pocketmine", new TeleportCommand("tp"));
-
-		$this->register("pocketmine", new TransferServerCommand("transfer"));
-
-		$this->register("pocketmine", new TimeCommand("time"));
-		$this->register("pocketmine", new TimingsCommand("timings"));
-		$this->register("pocketmine", new ReloadCommand("reload"), null, true);
-		$this->register("pocketmine", new XpCommand("xp"));
-		$this->register("pocketmine", new SetBlockCommand("setblock"));
+	    $this->registerAll("pocketmine", [
+            new BanCidByNameCommand("bancidbyname"),
+            new BanCidCommand("bancid"),
+            new BanCommand("ban"),
+            new BanIpByNameCommand("banipbyname"),
+            new BanIpCommand("ban-ip"),
+            new BanListCommand("banlist"),
+            new BiomeCommand("biome"),
+            new CaveCommand("cave"),
+            new ChunkInfoCommand("chunkinfo"),
+            new DefaultGamemodeCommand("defaultgamemode"),
+            new DeopCommand("deop"),
+            new DifficultyCommand("difficulty"),
+            new EffectCommand("effect"),
+            new EnchantCommand("enchant"),
+            new ExtractPharCommand("extractphar"),
+            new ExtractPluginCommand("ep"),
+            new FillCommand("fill"),
+            new GamemodeCommand("gamemode"),
+            new GiveCommand("give"),
+            new HelpCommand("help"),
+            new KickCommand("kick"),
+            new KillCommand("kill"),
+            new ListCommand("list"),
+            new LoadPluginCommand("loadplugin"),
+            new LvdatCommand("lvdat"),
+            new MakePluginCommand("mp"),
+            new MakeServerCommand("ms"),
+            new MeCommand("me"),
+            new OpCommand("op"),
+            new PardonCidCommand("pardoncid"),
+            new PardonCommand("pardon"),
+            new PardonIpCommand("pardon-ip"),
+            new ParticleCommand("particle"),
+            new PingCommand("ping"),
+            new PluginsCommand("plugins"),
+            new ReloadCommand("reload"),
+            new SaveCommand("save-all"),
+            new SaveOffCommand("save-off"),
+            new SaveOnCommand("save-on"),
+            new SayCommand("say"),
+            new SeedCommand("seed"),
+            new SetBlockCommand("setblock"),
+            new SetWorldSpawnCommand("setworldspawn"),
+            new SpawnpointCommand("spawnpoint"),
+            new StopCommand("stop"),
+            new SummonCommand("summon"),
+            new TeleportCommand("tp"),
+            new TellCommand("tell"),
+            new TimeCommand("time"),
+            new TimingsCommand("timings"),
+            new TransferServerCommand("transfer"),
+            new VersionCommand("version"),
+            new WeatherCommand("weather"),
+            new WhitelistCommand("whitelist"),
+            new XpCommand("xp"),
+        ]);
 
 		if($this->server->getProperty("debug.commands", false)){
-			$this->register("pocketmine", new StatusCommand("status"), null, true);
-			$this->register("pocketmine", new GarbageCollectorCommand("gc"), null, true);
-			$this->register("pocketmine", new DumpMemoryCommand("dumpmemory"), null, true);
+			$this->registerAll("pocketmine", [
+                new DumpMemoryCommand("dumpmemory"),
+                new GarbageCollectorCommand("gc"),
+                new StatusCommand("status")
+            ]);
 		}
 	}
 
@@ -204,18 +202,17 @@ class SimpleCommandMap implements CommandMap {
 	 * @param string  $fallbackPrefix
 	 * @param Command $command
 	 * @param null    $label
-	 * @param bool    $overrideConfig
 	 *
 	 * @return bool
 	 */
-	public function register($fallbackPrefix, Command $command, $label = null, $overrideConfig = false){
+	public function register($fallbackPrefix, Command $command, $label = null){
 		if($label === null){
 			$label = $command->getName();
 		}
 		$label = strtolower(trim($label));
 
 		//Check if command was disabled in config and for override
-		if(!(($this->commandConfig[$label] ?? $this->commandConfig["default"] ?? true) or $overrideConfig)){
+		if(!(($this->commandConfig[$label] ?? $this->commandConfig["default"] ?? true))){
 			return false;
 		}
 		$fallbackPrefix = strtolower(trim($fallbackPrefix));
@@ -238,6 +235,21 @@ class SimpleCommandMap implements CommandMap {
 
 		return $registered;
 	}
+
+    /**
+     * @param Command $command
+     *
+     * @return bool
+     */
+    public function unregister(Command $command) : bool{
+        foreach($this->knownCommands as $lbl => $cmd){
+            if($cmd === $command){
+                unset($this->knownCommands[$lbl]);
+            }
+        }
+        $command->unregister($this);
+        return true;
+    }
 
 	/**
 	 * @param Command $command
