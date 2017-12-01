98be29d7ea90d510433e76e60269a3fb7eea20f6
ENES <enes5519@gmail.com>
2017-12-02 00:15:43 +0300

    Added api

--- src/pocketmine/utils/Color.php
+++ src/pocketmine/utils/Color.php
@@ -26,94 +26,94 @@ namespace pocketmine\utils;
 
 class Color {
 
-	const COLOR_DYE_BLACK = 0;//dye colors
-	const COLOR_DYE_RED = 1;
-	const COLOR_DYE_GREEN = 2;
-	const COLOR_DYE_BROWN = 3;
-	const COLOR_DYE_BLUE = 4;
-	const COLOR_DYE_PURPLE = 5;
-	const COLOR_DYE_CYAN = 6;
-	const COLOR_DYE_LIGHT_GRAY = 7;
-	const COLOR_DYE_GRAY = 8;
-	const COLOR_DYE_PINK = 9;
-	const COLOR_DYE_LIME = 10;
-	const COLOR_DYE_YELLOW = 11;
-	const COLOR_DYE_LIGHT_BLUE = 12;
-	const COLOR_DYE_MAGENTA = 13;
-	const COLOR_DYE_ORANGE = 14;
-	const COLOR_DYE_WHITE = 15;
+    const COLOR_DYE_BLACK = 0;//dye colors
+    const COLOR_DYE_RED = 1;
+    const COLOR_DYE_GREEN = 2;
+    const COLOR_DYE_BROWN = 3;
+    const COLOR_DYE_BLUE = 4;
+    const COLOR_DYE_PURPLE = 5;
+    const COLOR_DYE_CYAN = 6;
+    const COLOR_DYE_LIGHT_GRAY = 7;
+    const COLOR_DYE_GRAY = 8;
+    const COLOR_DYE_PINK = 9;
+    const COLOR_DYE_LIME = 10;
+    const COLOR_DYE_YELLOW = 11;
+    const COLOR_DYE_LIGHT_BLUE = 12;
+    const COLOR_DYE_MAGENTA = 13;
+    const COLOR_DYE_ORANGE = 14;
+    const COLOR_DYE_WHITE = 15;
 
-	private $red = 0;
-	private $green = 0;
-	private $blue = 0;
+    private $red = 0;
+    private $green = 0;
+    private $blue = 0;
 
-	/** @var \SplFixedArray */
-	public static $dyeColors = null;
+    /** @var \SplFixedArray */
+    public static $dyeColors = null;
     private $alpha = 0;
 
     public static function init(){
-		if(self::$dyeColors === null){
-			self::$dyeColors = new \SplFixedArray(16); //What's the point of making a 256-long array for 16 objects?
-			self::$dyeColors[self::COLOR_DYE_BLACK] = Color::getRGB(30, 27, 27);
-			self::$dyeColors[self::COLOR_DYE_RED] = Color::getRGB(179, 49, 44);
-			self::$dyeColors[self::COLOR_DYE_GREEN] = Color::getRGB(61, 81, 26);
-			self::$dyeColors[self::COLOR_DYE_BROWN] = Color::getRGB(81, 48, 26);
-			self::$dyeColors[self::COLOR_DYE_BLUE] = Color::getRGB(37, 49, 146);
-			self::$dyeColors[self::COLOR_DYE_PURPLE] = Color::getRGB(123, 47, 190);
-			self::$dyeColors[self::COLOR_DYE_CYAN] = Color::getRGB(40, 118, 151);
-			self::$dyeColors[self::COLOR_DYE_LIGHT_GRAY] = Color::getRGB(153, 153, 153);
-			self::$dyeColors[self::COLOR_DYE_GRAY] = Color::getRGB(67, 67, 67);
-			self::$dyeColors[self::COLOR_DYE_PINK] = Color::getRGB(216, 129, 152);
-			self::$dyeColors[self::COLOR_DYE_LIME] = Color::getRGB(65, 205, 52);
-			self::$dyeColors[self::COLOR_DYE_YELLOW] = Color::getRGB(222, 207, 42);
-			self::$dyeColors[self::COLOR_DYE_LIGHT_BLUE] = Color::getRGB(102, 137, 211);
-			self::$dyeColors[self::COLOR_DYE_MAGENTA] = Color::getRGB(195, 84, 205);
-			self::$dyeColors[self::COLOR_DYE_ORANGE] = Color::getRGB(235, 136, 68);
-			self::$dyeColors[self::COLOR_DYE_WHITE] = Color::getRGB(240, 240, 240);
-		}
-	}
+        if(self::$dyeColors === null){
+            self::$dyeColors = new \SplFixedArray(16); //What's the point of making a 256-long array for 16 objects?
+            self::$dyeColors[self::COLOR_DYE_BLACK] = Color::getRGB(30, 27, 27);
+            self::$dyeColors[self::COLOR_DYE_RED] = Color::getRGB(179, 49, 44);
+            self::$dyeColors[self::COLOR_DYE_GREEN] = Color::getRGB(61, 81, 26);
+            self::$dyeColors[self::COLOR_DYE_BROWN] = Color::getRGB(81, 48, 26);
+            self::$dyeColors[self::COLOR_DYE_BLUE] = Color::getRGB(37, 49, 146);
+            self::$dyeColors[self::COLOR_DYE_PURPLE] = Color::getRGB(123, 47, 190);
+            self::$dyeColors[self::COLOR_DYE_CYAN] = Color::getRGB(40, 118, 151);
+            self::$dyeColors[self::COLOR_DYE_LIGHT_GRAY] = Color::getRGB(153, 153, 153);
+            self::$dyeColors[self::COLOR_DYE_GRAY] = Color::getRGB(67, 67, 67);
+            self::$dyeColors[self::COLOR_DYE_PINK] = Color::getRGB(216, 129, 152);
+            self::$dyeColors[self::COLOR_DYE_LIME] = Color::getRGB(65, 205, 52);
+            self::$dyeColors[self::COLOR_DYE_YELLOW] = Color::getRGB(222, 207, 42);
+            self::$dyeColors[self::COLOR_DYE_LIGHT_BLUE] = Color::getRGB(102, 137, 211);
+            self::$dyeColors[self::COLOR_DYE_MAGENTA] = Color::getRGB(195, 84, 205);
+            self::$dyeColors[self::COLOR_DYE_ORANGE] = Color::getRGB(235, 136, 68);
+            self::$dyeColors[self::COLOR_DYE_WHITE] = Color::getRGB(240, 240, 240);
+        }
+    }
 
-	/**
-	 * @param $r
-	 * @param $g
-	 * @param $b
-	 *
-	 * @return Color
-	 */
-	public static function getRGB($r, $g, $b){
-		return new Color((int) $r, (int) $g, (int) $b);
-	}
+    /**
+     * @param $r
+     * @param $g
+     * @param $b
+     *
+     * @return Color
+     */
+    public static function getRGB($r, $g, $b){
+        return new Color((int) $r, (int) $g, (int) $b);
+    }
 
-	/**
-	 * @param Color[] ...$colors
-	 *
-	 * @return Color
-	 */
-	public static function averageColor(Color ...$colors){
-		$tr = 0;//total red
-		$tg = 0;//green
-		$tb = 0;//blue
-		$count = 0;
-		foreach($colors as $c){
-			$tr += $c->getRed();
-			$tg += $c->getGreen();
-			$tb += $c->getBlue();
-			++$count;
-		}
-		return Color::getRGB($tr / $count, $tg / $count, $tb / $count);
-	}
+    /**
+     * @param Color[] ...$colors
+     *
+     * @return Color
+     */
+    public static function averageColor(Color ...$colors){
+        $tr = 0;//total red
+        $tg = 0;//green
+        $tb = 0;//blue
+        $count = 0;
+        foreach($colors as $c){
+            $tr += $c->getRed();
+            $tg += $c->getGreen();
+            $tb += $c->getBlue();
+            ++$count;
+        }
+        return Color::getRGB($tr / $count, $tg / $count, $tb / $count);
+    }
 
-	/**
-	 * @param $id
-	 *
-	 * @return mixed|Color
-	 */
-	public static function getDyeColor($id){
-		if(isset(self::$dyeColors[$id])){
-			return clone self::$dyeColors[$id];
-		}
-		return Color::getRGB(0, 0, 0);
-	}
+    /**
+     * @param $id
+     *
+     * @return mixed|Color
+     */
+    public static function getDyeColor($id){
+        if(isset(self::$dyeColors[$id])){
+            return clone self::$dyeColors[$id];
+        }
+        return Color::getRGB(0, 0, 0);
+    }
 
     /**
      * Color constructor.
@@ -123,12 +123,12 @@ class Color {
      * @param $b
      * @param int $a
      */
-	public function __construct($r, $g, $b, $a = 0xff){
-		$this->red = $r & 0xff;
-		$this->green = $g & 0xff;
-		$this->blue = $b & 0xff;
+    public function __construct($r, $g, $b, $a = 0xff){
+        $this->red = $r & 0xff;
+        $this->green = $g & 0xff;
+        $this->blue = $b & 0xff;
         $this->alpha = $a & 0xff;
-	}
+    }
 
     /**
      * Returns the alpha (transparency) value of this colour.
@@ -146,47 +146,72 @@ class Color {
         $this->alpha = $a & 0xff;
     }
 
-	/**
-	 * @return int
-	 */
-	public function getRed(){
-		return (int) $this->red;
-	}
+    /**
+     * @return int
+     */
+    public function getRed(){
+        return (int) $this->red;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getBlue(){
-		return (int) $this->blue;
-	}
+    /**
+     * @return int
+     */
+    public function getBlue(){
+        return (int) $this->blue;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getGreen(){
-		return (int) $this->green;
-	}
+    /**
+     * @return int
+     */
+    public function getGreen(){
+        return (int) $this->green;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getColorCode(){
-		return ($this->red << 16 | $this->green << 8 | $this->blue) & 0xffffff;
-	}
+    /**
+     * @return int
+     */
+    public function getR(){
+        return $this->getRed();
+    }
+
+    /**
+     * @return int
+     */
+    public function getG(){
+        return $this->getGreen();
+    }
+
+    /**
+     * @return int
+     */
+    public function getB(){
+        return $this->getBlue();
+    }
+
+    public function getA(){
+        return $this->getAlpha();
+    }
+
+    /**
+     * @return int
+     */
+    public function getColorCode(){
+        return ($this->red << 16 | $this->green << 8 | $this->blue) & 0xffffff;
+    }
 
     /**
      * @return int
      */
     public function toRGB() : int{
         return ($this->red << 16) | ($this->green << 8) | $this->blue;
- 	}
+    }
 
-	/**
-	 * @return string
-	 */
-	public function __toString(){
-		return "Color(red:" . $this->red . ", green:" . $this->green . ", blue:" . $this->blue . ")";
-	}
+    /**
+     * @return string
+     */
+    public function __toString(){
+        return "Color(red:" . $this->red . ", green:" . $this->green . ", blue:" . $this->blue . ")";
+    }
 
     /**
      * Returns a Color from the supplied RGB colour code (24-bit)
