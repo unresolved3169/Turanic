ce5c3bf94c92ac94f35942860de4e7ff4fcf34a7
EmreTr1 <emre.yavuz.842@gmail.com>
2017-12-25 22:24:05 +0300

    Fixed Lava Attack

--- src/pocketmine/block/Lava.php
+++ src/pocketmine/block/Lava.php
@@ -65,7 +65,7 @@ class Lava extends Liquid {
 		$ProtectL = 0;
 		if(!$entity->hasEffect(Effect::FIRE_RESISTANCE)){
 			$ev = new EntityDamageByBlockEvent($this, $entity, EntityDamageEvent::CAUSE_LAVA, 4);
-			if($entity->attack($ev->getFinalDamage(), $ev) === true){
+			if($entity->attack($ev) === true){
 				$ev->useArmors();
 			}
 			$ProtectL = $ev->getFireProtectL();
