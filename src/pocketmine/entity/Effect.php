edd3f88bf6f13ef3c5ff4b2c34379514733adeba
ENES <enes5519@gmail.com>
2017-12-30 14:49:00 +0300

    Effect: Add constants with vanilla names

--- src/pocketmine/entity/Effect.php
+++ src/pocketmine/entity/Effect.php
@@ -39,11 +39,11 @@ class Effect {
 	const STRENGTH = 5;
 	const HEALING = 6;
 	const HARMING = 7;
-	const JUMP = 8;
+	const JUMP_BOOST = 8, JUMP = 8;
 	const NAUSEA = 9;
 	const CONFUSION = 9;
 	const REGENERATION = 10;
-	const DAMAGE_RESISTANCE = 11;
+	const RESISTANCE = 11, DAMAGE_RESISTANCE = 11;
 	const FIRE_RESISTANCE = 12;
 	const WATER_BREATHING = 13;
 	const INVISIBILITY = 14;
