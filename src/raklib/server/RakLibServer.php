5f084805d73261324a2e1e9950adb33b7fd0c95a
Enes Yıldırım <enes5519@gmail.com>
2018-01-30 15:52:54 +0100

    Fix RaklibServer

--- src/raklib/server/RakLibServer.php
+++ src/raklib/server/RakLibServer.php
@@ -20,207 +20,206 @@ namespace raklib\server;
 use raklib\utils\InternetAddress;
 
 class RakLibServer extends \Thread{
-	/** @var InternetAddress */
-	private $address;
+    /** @var InternetAddress */
+    private $address;
 
-	/** @var \ThreadedLogger */
-	protected $logger;
+    /** @var \ThreadedLogger */
+    protected $logger;
 
-	/** @var string */
-	protected $loaderPath;
+    /** @var \ClassLoader */
+    protected $loader;
 
-	/** @var bool */
-	protected $shutdown = false;
+    /** @var bool */
+    protected $shutdown = false;
 
-	/** @var \Threaded */
-	protected $externalQueue;
-	/** @var \Threaded */
-	protected $internalQueue;
+    /** @var \Threaded */
+    protected $externalQueue;
+    /** @var \Threaded */
+    protected $internalQueue;
 
-	/** @var string */
-	protected $mainPath;
+    /** @var string */
+    protected $mainPath;
 
-	/** @var int */
-	protected $serverId = 0;
-	/** @var int */
-	protected $maxMtuSize;
-
-
-	/**
-	 * @param \ThreadedLogger $logger
-	 * @param string          $autoloaderPath Path to Composer autoloader
-	 * @param InternetAddress $address
-	 * @param int             $maxMtuSize
-	 *
-	 * @throws \Exception
-	 */
-	public function __construct(\ThreadedLogger $logger, string $autoloaderPath, InternetAddress $address, int $maxMtuSize = 1492){
-		$this->address = $address;
-
-		$this->serverId = mt_rand(0, PHP_INT_MAX);
-		$this->maxMtuSize = $maxMtuSize;
-
-		$this->logger = $logger;
-		$this->loaderPath = $autoloaderPath;
-
-		$this->externalQueue = new \Threaded;
-		$this->internalQueue = new \Threaded;
-
-		if(\Phar::running(true) !== ""){
-			$this->mainPath = \Phar::running(true);
-		}else{
-			$this->mainPath = \realpath(\getcwd()) . DIRECTORY_SEPARATOR;
-		}
-	}
-
-	public function isShutdown() : bool{
-		return $this->shutdown === true;
-	}
-
-	public function shutdown() : void{
-		$this->shutdown = true;
-	}
-
-	/**
-	 * Returns the RakNet server ID
-	 * @return int
-	 */
-	public function getServerId() : int{
-		return $this->serverId;
-	}
-
-	/**
-	 * @return \ThreadedLogger
-	 */
-	public function getLogger() : \ThreadedLogger{
-		return $this->logger;
-	}
-
-	/**
-	 * @return \Threaded
-	 */
-	public function getExternalQueue() : \Threaded{
-		return $this->externalQueue;
-	}
-
-	/**
-	 * @return \Threaded
-	 */
-	public function getInternalQueue() : \Threaded{
-		return $this->internalQueue;
-	}
-
-	public function pushMainToThreadPacket(string $str) : void{
-		$this->internalQueue[] = $str;
-	}
-
-	public function readMainToThreadPacket() : ?string{
-		return $this->internalQueue->shift();
-	}
-
-	public function pushThreadToMainPacket(string $str) : void{
-		$this->externalQueue[] = $str;
-	}
-
-	public function readThreadToMainPacket() : ?string{
-		return $this->externalQueue->shift();
-	}
-
-	public function shutdownHandler(){
-		if($this->shutdown !== true){
-			$this->getLogger()->emergency("RakLib crashed!");
-		}
-	}
-
-	public function errorHandler($errno, $errstr, $errfile, $errline){
-		if(error_reporting() === 0){
-			return false;
-		}
-
-		$errorConversion = [
-			E_ERROR => "E_ERROR",
-			E_WARNING => "E_WARNING",
-			E_PARSE => "E_PARSE",
-			E_NOTICE => "E_NOTICE",
-			E_CORE_ERROR => "E_CORE_ERROR",
-			E_CORE_WARNING => "E_CORE_WARNING",
-			E_COMPILE_ERROR => "E_COMPILE_ERROR",
-			E_COMPILE_WARNING => "E_COMPILE_WARNING",
-			E_USER_ERROR => "E_USER_ERROR",
-			E_USER_WARNING => "E_USER_WARNING",
-			E_USER_NOTICE => "E_USER_NOTICE",
-			E_STRICT => "E_STRICT",
-			E_RECOVERABLE_ERROR => "E_RECOVERABLE_ERROR",
-			E_DEPRECATED => "E_DEPRECATED",
-			E_USER_DEPRECATED => "E_USER_DEPRECATED"
-		];
-
-		$errno = $errorConversion[$errno] ?? $errno;
-
-		$errstr = preg_replace('/\s+/', ' ', trim($errstr));
-		$errfile = $this->cleanPath($errfile);
-
-		$this->getLogger()->debug("An $errno error happened: \"$errstr\" in \"$errfile\" at line $errline");
-
-		foreach($this->getTrace(2) as $i => $line){
-			$this->getLogger()->debug($line);
-		}
-
-		return true;
-	}
-
-	public function getTrace($start = 0, $trace = null){
-		if($trace === null){
-			if(function_exists("xdebug_get_function_stack")){
-				$trace = array_reverse(xdebug_get_function_stack());
-			}else{
-				$e = new \Exception();
-				$trace = $e->getTrace();
-			}
-		}
-
-		$messages = [];
-		$j = 0;
-		for($i = (int) $start; isset($trace[$i]); ++$i, ++$j){
-			$params = "";
-			if(isset($trace[$i]["args"]) or isset($trace[$i]["params"])){
-				if(isset($trace[$i]["args"])){
-					$args = $trace[$i]["args"];
-				}else{
-					$args = $trace[$i]["params"];
-				}
-				foreach($args as $name => $value){
-					$params .= (is_object($value) ? get_class($value) . " " . (method_exists($value, "__toString") ? $value->__toString() : "object") : gettype($value) . " " . @strval($value)) . ", ";
-				}
-			}
-			$messages[] = "#$j " . (isset($trace[$i]["file"]) ? $this->cleanPath($trace[$i]["file"]) : "") . "(" . (isset($trace[$i]["line"]) ? $trace[$i]["line"] : "") . "): " . (isset($trace[$i]["class"]) ? $trace[$i]["class"] . (($trace[$i]["type"] === "dynamic" or $trace[$i]["type"] === "->") ? "->" : "::") : "") . $trace[$i]["function"] . "(" . substr($params, 0, -2) . ")";
-		}
-
-		return $messages;
-	}
-
-	public function cleanPath($path){
-		return str_replace(["\\", ".php", "phar://", str_replace(["\\", "phar://"], ["/", ""], $this->mainPath)], ["/", "", "", ""], $path);
-	}
-
-	public function run() : void{
-		try{
-			require $this->loaderPath;
-
-			gc_enable();
-			error_reporting(-1);
-			ini_set("display_errors", '1');
-			ini_set("display_startup_errors", '1');
-
-			set_error_handler([$this, "errorHandler"], E_ALL);
-			register_shutdown_function([$this, "shutdownHandler"]);
+    /** @var int */
+    protected $serverId = 0;
+    /** @var int */
+    protected $maxMtuSize;
 
 
-			$socket = new UDPServerSocket($this->address);
-			new SessionManager($this, $socket, $this->maxMtuSize);
-		}catch(\Throwable $e){
-			$this->logger->logException($e);
-		}
-	}
+    /**
+     * @param \ThreadedLogger $logger
+     * @param \ClassLoader $loader
+     * @param InternetAddress $address
+     * @param int $maxMtuSize
+     *
+     */
+    public function __construct(\ThreadedLogger $logger, \ClassLoader $loader, InternetAddress $address, int $maxMtuSize = 1492){
+        $this->address = $address;
+
+        $this->serverId = mt_rand(0, PHP_INT_MAX);
+        $this->maxMtuSize = $maxMtuSize;
+
+        $this->logger = $logger;
+        $this->loader = $loader;
+
+        $this->externalQueue = new \Threaded;
+        $this->internalQueue = new \Threaded;
+
+        if(\Phar::running(true) !== ""){
+            $this->mainPath = \Phar::running(true);
+        }else{
+            $this->mainPath = \realpath(\getcwd()) . DIRECTORY_SEPARATOR;
+        }
+    }
+
+    public function isShutdown() : bool{
+        return $this->shutdown === true;
+    }
+
+    public function shutdown(){
+        $this->shutdown = true;
+    }
+
+    /**
+     * Returns the RakNet server ID
+     * @return int
+     */
+    public function getServerId() : int{
+        return $this->serverId;
+    }
+
+    /**
+     * @return \ThreadedLogger
+     */
+    public function getLogger() : \ThreadedLogger{
+        return $this->logger;
+    }
+
+    /**
+     * @return \Threaded
+     */
+    public function getExternalQueue() : \Threaded{
+        return $this->externalQueue;
+    }
+
+    /**
+     * @return \Threaded
+     */
+    public function getInternalQueue() : \Threaded{
+        return $this->internalQueue;
+    }
+
+    public function pushMainToThreadPacket(string $str){
+        $this->internalQueue[] = $str;
+    }
+
+    public function readMainToThreadPacket(){
+        return $this->internalQueue->shift();
+    }
+
+    public function pushThreadToMainPacket(string $str){
+        $this->externalQueue[] = $str;
+    }
+
+    public function readThreadToMainPacket(){
+        return $this->externalQueue->shift();
+    }
+
+    public function shutdownHandler(){
+        if($this->shutdown !== true){
+            $this->getLogger()->emergency("RakLib crashed!");
+        }
+    }
+
+    public function errorHandler($errno, $errstr, $errfile, $errline){
+        if(error_reporting() === 0){
+            return false;
+        }
+
+        $errorConversion = [
+            E_ERROR => "E_ERROR",
+            E_WARNING => "E_WARNING",
+            E_PARSE => "E_PARSE",
+            E_NOTICE => "E_NOTICE",
+            E_CORE_ERROR => "E_CORE_ERROR",
+            E_CORE_WARNING => "E_CORE_WARNING",
+            E_COMPILE_ERROR => "E_COMPILE_ERROR",
+            E_COMPILE_WARNING => "E_COMPILE_WARNING",
+            E_USER_ERROR => "E_USER_ERROR",
+            E_USER_WARNING => "E_USER_WARNING",
+            E_USER_NOTICE => "E_USER_NOTICE",
+            E_STRICT => "E_STRICT",
+            E_RECOVERABLE_ERROR => "E_RECOVERABLE_ERROR",
+            E_DEPRECATED => "E_DEPRECATED",
+            E_USER_DEPRECATED => "E_USER_DEPRECATED"
+        ];
+
+        $errno = $errorConversion[$errno] ?? $errno;
+
+        $errstr = preg_replace('/\s+/', ' ', trim($errstr));
+        $errfile = $this->cleanPath($errfile);
+
+        $this->getLogger()->debug("An $errno error happened: \"$errstr\" in \"$errfile\" at line $errline");
+
+        foreach($this->getTrace(2) as $i => $line){
+            $this->getLogger()->debug($line);
+        }
+
+        return true;
+    }
+
+    public function getTrace($start = 0, $trace = null){
+        if($trace === null){
+            if(function_exists("xdebug_get_function_stack")){
+                $trace = array_reverse(xdebug_get_function_stack());
+            }else{
+                $e = new \Exception();
+                $trace = $e->getTrace();
+            }
+        }
+
+        $messages = [];
+        $j = 0;
+        for($i = (int) $start; isset($trace[$i]); ++$i, ++$j){
+            $params = "";
+            if(isset($trace[$i]["args"]) or isset($trace[$i]["params"])){
+                if(isset($trace[$i]["args"])){
+                    $args = $trace[$i]["args"];
+                }else{
+                    $args = $trace[$i]["params"];
+                }
+                foreach($args as $name => $value){
+                    $params .= (is_object($value) ? get_class($value) . " " . (method_exists($value, "__toString") ? $value->__toString() : "object") : gettype($value) . " " . @strval($value)) . ", ";
+                }
+            }
+            $messages[] = "#$j " . (isset($trace[$i]["file"]) ? $this->cleanPath($trace[$i]["file"]) : "") . "(" . (isset($trace[$i]["line"]) ? $trace[$i]["line"] : "") . "): " . (isset($trace[$i]["class"]) ? $trace[$i]["class"] . (($trace[$i]["type"] === "dynamic" or $trace[$i]["type"] === "->") ? "->" : "::") : "") . $trace[$i]["function"] . "(" . substr($params, 0, -2) . ")";
+        }
+
+        return $messages;
+    }
+
+    public function cleanPath($path){
+        return str_replace(["\\", ".php", "phar://", str_replace(["\\", "phar://"], ["/", ""], $this->mainPath)], ["/", "", "", ""], $path);
+    }
+
+    public function run() : void{
+        try{
+            $this->loader->register(true);
+
+            gc_enable();
+            error_reporting(-1);
+            ini_set("display_errors", '1');
+            ini_set("display_startup_errors", '1');
+
+            set_error_handler([$this, "errorHandler"], E_ALL);
+            register_shutdown_function([$this, "shutdownHandler"]);
+
+
+            $socket = new UDPServerSocket($this->address);
+            new SessionManager($this, $socket, $this->maxMtuSize);
+        }catch(\Throwable $e){
+            $this->logger->logException($e);
+        }
+    }
 
 }
