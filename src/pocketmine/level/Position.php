e4020038597e91b3c6c67f8ddd3fd80208ab5677
Enes Yıldırım <enes5519@gmail.com>
2018-01-30 13:47:44 +0100

    Fix Position

--- src/pocketmine/level/Position.php
+++ src/pocketmine/level/Position.php
@@ -120,10 +120,6 @@ class Position extends Vector3 {
         return "Position(level=" . ($this->isValid() ? $this->getLevel()->getName() : "null") . ",x=" . $this->x . ",y=" . $this->y . ",z=" . $this->z . ")";
     }
 
-    public function toArray(){
-        return [$this->x, $this->y, $this->z];
-    }
-
     public function equals(Vector3 $v) : bool{
         if($v instanceof Position){
             return parent::equals($v) and $v->getLevel() === $this->getLevel();
